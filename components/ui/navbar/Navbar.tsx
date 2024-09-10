429abff50aed1a094b1abc3ebebc2492709c904f
mahmoudbahaa755 <mahmoudbahaa755@gmail.com>
2024-09-10 21:36:39 +0300

    improve(navBar): improving css

--- components/ui/navbar/Navbar.tsx
+++ components/ui/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import React from "react";
-import { FaChevronDown, FaHome, FaUser } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { FaHome } from "react-icons/fa";
+import { MdArrowDropDown } from "react-icons/md";
 import logo from "../../../public/logo.png";
 
 const Navbar: React.FC = () => {
@@ -40,8 +42,9 @@ const Navbar: React.FC = () => {
           </span>
         </button>
         <button className="gap-2 !text-primary rounded-full flex items-center w-[102px] h-[31px] justify-center">
-          <FaUser className="!text-primary rounded-full ring-2 ring-primary" />{" "}
+          <CgProfile className="!text-primary rounded-full ring-2 ring-primary" />{" "}
           <span
+            className="text-[9.75px] text-[#EF3F24]"
             style={{
               color: "#EF3F24",
             }}
@@ -50,13 +53,13 @@ const Navbar: React.FC = () => {
           </span>
         </button>
         <button className="  !text-primary !text-[#EF3F24] rounded-full  flex items-center w-[102px] h-[31px] justify-center">
-          <FaChevronDown />
+          <MdArrowDropDown className="w-[9.25px] h-[4.63px]" size={10} />
           <span
             style={{
               color: "#EF3F24",
             }}
+            className="text-[9.75px] text-[#EF3F24]"
           >
-            {" "}
             En
           </span>
         </button>
