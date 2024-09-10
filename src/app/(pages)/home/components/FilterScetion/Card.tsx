805ebc19196577a3ae7df634ba41238a394da5e1
mahmoudbahaa755 <mahmoudbahaa755@gmail.com>
2024-09-10 23:33:33 +0300

    fix img import

--- src/app/(pages)/home/components/FilterScetion/Card.tsx
+++ src/app/(pages)/home/components/FilterScetion/Card.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
-import defaultImg from "../../../../../public/loc.webp";
+import defaultImg from "../../../../../../public/loc.webp";
 export default function Card({
   img,
   title,
