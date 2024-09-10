4d6dbbe0d954880df2624a0c28c1d955087fcc98
mahmoudbahaa755 <mahmoudbahaa755@gmail.com>
2024-09-10 22:53:56 +0300

    change build icon

--- components/ui/navbar/Navbar.tsx
+++ components/ui/navbar/Navbar.tsx
@@ -1,42 +1,30 @@
 import Image from "next/image";
 import React from "react";
-import { FaHome } from "react-icons/fa";
+import { FaRegBuilding } from "react-icons/fa";
 import { MdArrowDropDown } from "react-icons/md";
 import { TiUser } from "react-icons/ti";
 import logo from "../../../public/logo.png";
 
 const Navbar: React.FC = () => {
   return (
-    <div className=" flex align-baseline pt-4 flex-row-reverse !bg-white   justify-between ">
-      <div
-        style={{
-          background: " 0% 0% no-repeat padding-box",
-          opacity: 1,
-        }}
-      >
-        <Image width={164.9} height={32.22} src={logo} alt="logo" />
-        {/* aroovia <span className="text-[#78BAC6]">tm</span> */}
-      </div>
+    <div className="flex items-center justify-between pt-4 bg-white">
+      <Image width={164.9} height={32.22} src={logo} alt="logo" />
 
-      <div className="flex justify-center gap-2 items-center  ">
+      <div className="flex items-center gap-2">
         <div>
-          {" "}
           <button
-            className="  flex  gap-[4px] !text-white"
             style={{
               background: "#78BAC6 0% 0% no-repeat padding-box ",
               borderRadius: "32px",
               width: "108px",
               height: "31px",
-              // padding: "2.6px 12px",
-              opacity: 1,
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
               gap: "6px",
             }}
           >
-            <FaHome />
+            <FaRegBuilding fill="white" />
             <span
               style={{
                 fontSize: "11px",
@@ -61,7 +49,11 @@ const Navbar: React.FC = () => {
           </span>
         </div>
         <button className="   !text-[#EF3F24] rounded-full  flex items-center w-[102px] h-[31px] justify-center">
-          <MdArrowDropDown fill="#EF3F24" className="w-[9.25px] h-[4.63px]" />
+          <MdArrowDropDown
+            fill=""
+            className="w-[9.25px] h-[4.63px]"
+            size={10}
+          />
           <span
             style={{
               color: "#EF3F24",
