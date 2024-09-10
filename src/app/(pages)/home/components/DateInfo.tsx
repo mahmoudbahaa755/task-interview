5e2127c8df05111f99ae484d9453c21f9943d31a
mahmoudbahaa755 <mahmoudbahaa755@gmail.com>
2024-09-10 21:55:49 +0300

    fix(date): fix css

--- src/app/(pages)/home/components/DateInfo.tsx
+++ src/app/(pages)/home/components/DateInfo.tsx
@@ -70,13 +70,13 @@ function DateDetails({
   return (
     <div className="flex flex-col items-start justify-center h-full">
       <div className="text-[9.75px] text-gray-500 mb-2"> {title}</div>
-      <button className="  rounded-full flex items-center justify-between w-full">
+      <button className="  gap-2 rounded-full flex items-center justify-between w-full">
         <div className="text-left font-bold text-[21.75px]  text-black">
           {date}
         </div>
-        <BsCalendarDateFill className="ml-[11.07px]" />
+        <BsCalendarDateFill className="w-[11.07px] h-[13.41px]" />
       </button>
-      <div className="flex items-center gap-2 ">
+      <div className="flex gap-2 items-center  ">
         <div className="text-[9.75px]">
           <a className="">السابق</a>
           <div className="underline w-[25px] rounded-sm h-[0.9px] bg-black"></div>
