68ca7ab3097dcfd3531cad291f37593064e32c9c
mahmoudbahaa755 <mahmoudbahaa755@gmail.com>
2024-09-11 21:37:41 +0300

    config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,11 +17,11 @@ const config: Config = {
           xl: "1280px",
         },
         padding: {
-          DEFAULT: "1rem",
-          sm: "2rem",
-          lg: "3rem",
-          xl: "3rem",
-          "2xl": "6rem",
+          DEFAULT: "0.5rem",
+          sm: "1rem",
+          lg: "2rem",
+          xl: "2rem",
+          "2xl": "4rem",
         },
       },
       colors: {
