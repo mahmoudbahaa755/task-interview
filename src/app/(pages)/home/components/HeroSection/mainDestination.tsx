5db878f47075cfa0001c08a13b3a2636d20b91ae
mahmoudbahaa755 <mahmoudbahaa755@gmail.com>
2024-09-11 17:20:47 +0300

    fix

--- src/app/(pages)/home/components/HeroSection/mainDestination.tsx
+++ src/app/(pages)/home/components/HeroSection/mainDestination.tsx
@@ -1,49 +1,17 @@
 "use client";
-import { useGlobalLoading } from "../../../../../../utils/Loading";
 import BoxContainer from "../container/BoxContainer";
 
 export default function MainDestination() {
-  const loading = useGlobalLoading();
-  console.log(loading);
   return (
     <BoxContainer>
       <div className="flex flex-col  items-start gap-1 justify-center h-full">
-        <div
-          // style={{
-          //   textAlign: "start",
-          //   fontWeight: "medium",
-          //   letterSpacing: "0px",
-          //   color: "#000000",
-          //   fontSize: "9.75px",
-          // }}
-          className="text-start font-medium text-[9.75px]  "
-        >
+        <div className="text-start font-medium text-[9.75px]  ">
           ابحث عن وجهة
         </div>
-        <div
-          // style={{
-          //   textAlign: "start",
-          //   fontWeight: "bold",
-          //   fontSize: "21.75px",
-          //   letterSpacing: "0px",
-          //   color: "#000000",
-          //   opacity: 1,
-          // }}
-          className="text-start font-bold text-[21.75px]  "
-        >
+        <div className="text-start font-bold text-[21.75px]  ">
           هايسيندا باي
         </div>
-        <div
-          // style={{
-          //   textAlign: "right",
-          //   fontWeight: "medium",
-          //   letterSpacing: "0px",
-          //   color: "#000000",
-          //   opacity: 1,
-          //   fontSize: "9.75px",
-          // }}
-          className="text-right font-medium text-[10px]  "
-        >
+        <div className="text-right font-medium text-[10px]  ">
           سيدي عبدالرحمن، مرسى مطروح، مصر
         </div>
       </div>
