795f9458b4cf3e6000d42f2cd151bd9941a4c75c
mahmoudbahaa755 <mahmoudbahaa755@gmail.com>
2024-09-11 17:17:39 +0300

    improving ui

--- src/app/(pages)/home/components/container/MainHeroContainer.tsx
+++ src/app/(pages)/home/components/container/MainHeroContainer.tsx
@@ -9,27 +9,24 @@ export default function MainHeroContainer() {
       style={{
         background: "#FFFFFF 0% 0% no-repeat padding-box",
         boxShadow: "0px 0px 40px #00000029",
-        borderRadius: "25px",
-        opacity: 1,
-        padding: "40px",
-        position: "relative",
-
-        width: "1124px",
-        height: "232px",
+        // width: "1124px",
+        // height: "232px",
       }}
+      className="grid relative md:grid-cols-1 lg:grid-cols-3 gap-[15px] rounded-[25px] p-[40px]"
     >
-      <div className="grid grid-cols-3 gap-[15px]">
-        <MainDestination />
-        <DateInfo />
-        <GuestInfo />
-      </div>
+      {/* <div className="grid grid-cols-3 gap-[15px]"> */}
+      <MainDestination />
+      <DateInfo />
+      <GuestInfo />
+      {/* </div> */}
       <div
-        style={{
-          position: "absolute",
-          bottom: "-55px",
-          left: "50%",
-          transform: "translateX(-50%)",
-        }}
+        // style={{
+        //   position: "absolute",
+        //   bottom: "-55px",
+        //   left: "50%",
+        //   transform: "translateX(-50%)",
+        // }}
+        className="absolute bottom-[-55px] left-[50%] transform -translate-x-1/2"
       >
         <SearchIcon />
       </div>
